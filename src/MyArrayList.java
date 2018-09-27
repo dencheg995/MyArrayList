3771007c976226f8610781249e37800b5625337b
denisgrebenuk <dencheg995@gmail.com>
2018-09-27 20:40:59 +0300

    implements List<E> and override almost all methods

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,29 +1,138 @@
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
-public class MyArrayList<E> implements MyList<E> {
-private int INIT_SIZE = 16;
+public class MyArrayList<E> implements List<E> {
+private int INIT_SIZE = 20;
 private Object[] ArrList = new Object[INIT_SIZE];
 private int point = 0; //size ArrayList
 
 
     @Override
-    public void add(E element) {
+    public boolean add(E element) {
     if(point == ArrList.length){
         resize(ArrList.length * 2);
     }
         ArrList[point++] = element;
+        return true;
     }
 
-    public void add(int index, E element){
-        if(point == ArrList.length){
-            resize(ArrList.length * 2);
+    @Override
+    public boolean remove(Object o) {
+        if(o == null) {
+            for (int i = 0; i < point; i++) {
+                if (ArrList[i] == null) {
+                    System.arraycopy(ArrList, i + 1, ArrList, i, point - i);
+                    ArrList[--point]=null;
+                    return true;
+                }
+            }
+        }
+        else {
+            for(int i = 0; i < point; i++){
+                if(o.equals(ArrList[i])){
+                    ArrList[i] = null;
+                    System.arraycopy(ArrList, i+1, ArrList, i, point - i);
+                    ArrList[--point]=null;
+                    return true;
+                }
+
+            }
         }
 
-        System.arraycopy(ArrList, index, ArrList, index + 1, point - index);
-        ArrList[index] = element;
-        point++;
-
+        return false;
     }
+
+    @Override
+    public int indexOf(Object o) {
+        if(o == null){
+            for(int i = 0; i < point; i++){
+                ArrList[i] = null;
+                return i;
+            }
+
+        }
+        else {
+            for(int i = 0; i < point; i++){
+                if(o.equals(ArrList[i])){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        int ind = 0;
+        for(int i = 0; i < point; i++) {
+            ind++;
+            if(c.iterator().next().equals(ArrList[i]) & ind == point) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        Object[] tempArr = c.toArray();
+        int length = tempArr.length;
+        System.arraycopy(tempArr, 0, ArrList, point, length);
+        point += length;
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        Object[] tempArr = c.toArray();
+        int length = tempArr.length;
+
+        if(index == 0) {
+            System.arraycopy(tempArr, 0, ArrList, point + length, length);
+        }
+        else {
+            System.arraycopy(tempArr, index, ArrList, point + length, point - index);
+        }
+        point += length;
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        int w = 0;
+        for(int i = 0; i < point; i++) {
+            if (c.contains(ArrList[i])) {
+                w++;
+                System.arraycopy(ArrList, w , ArrList, i, point - i);
+            }
+            ArrList[point--] = null;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        int w = 0;
+        for(int i  = 0; i < point; i++) {
+             if(!c.contains(ArrList[i])) {
+                 if(i == 0) {
+                    removeZero(0);
+                 }
+
+                 System.arraycopy(ArrList, i + 1, ArrList, i, point - i);
+                 point--;
+                 }
+
+
+             }
+        return true;
+        }
+
+
+
 
     public int size(){
         return point;
@@ -35,37 +144,55 @@ private int point = 0; //size ArrayList
         ArrList = ArrayList;
     }
 
-    public Object get(int index){
-        return ArrList[index];
+    public E get(int index){
+        return (E) ArrList[index];
     }
 
-    public void remove(int index){
-       for(int i = index; i < point; i++){
-           ArrList[i] = ArrList[i+1];
-       }
-       ArrList[point] = null;
-       point--;
-    }
-
-    public void remove(E element){
-        for(int i = 0; i < point; i++){
-            if(element.equals(ArrList[i])){
-                ArrList[i] = null;
-
-                System.arraycopy(ArrList, i+1, ArrList, i, point - i);
-
+    @Override
+    public int lastIndexOf(Object o) {
+        if(o == null) {
+            for(int i = point -1 ; i < 0; i--) {
+                if(ArrList[i] == null) {
+                   return i;
+                }
             }
+
         }
-    point--;
+        else {
+            for(int i = point - 1; i < 0; i--) {
+                if(o.equals(ArrList[i])){
+                   return i;
+                }
+            }
+
+        }
+        return -1;
     }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
 
     public boolean isEmpty(){
         return point == 0;
 
     }
 
-    public boolean contains(E element){
-        if(element == null) {
+    @Override
+    public boolean contains(Object o) {
+        if(o == null) {
             for (int i = 0; i < point; i++) {
                 if(ArrList[i] == null) {
                     return true;
@@ -76,13 +203,33 @@ private int point = 0; //size ArrayList
             }
         }
         else {
-          for(int i = 0; i < point; i++)  {
-              if(element.equals(ArrList[i]))
-                  return true;
-          }
+            for(int i = 0; i < point; i++)  {
+                if(o.equals(ArrList[i]))
+                    return true;
+            }
         }
-    return false;
+        return false;
     }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[point];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        for(int i = 0; i < point; i++) {
+      //      a[i] = ArrList[i];
+        }
+
+        return null;
+    }
+
 
     public void clear(){
         for(int i = 0; i < point; i++) {
@@ -92,30 +239,45 @@ private int point = 0; //size ArrayList
         point = 0;
     }
 
-public int indexOf(E element){
-        if(element == null){
-            for(int i = 0; i < point; i++){
-                ArrList[i] = null;
-                return i;
-            }
-
-        }
-        else {
-            for(int i = 0; i < point; i++){
-                if(element.equals(ArrList[i])){
-                    return i;
-                }
-            }
-        }
-        return -1;
-}
-
-public void set(int index, E element){
+    public E set(int index, E element){
         for(int i = 0; i < point; i++){
             if(i == index){
                 ArrList[i] = element;
             }
         }
+    return element;
 }
+
+    @Override
+    public void add(int index, E element) {
+        if(point == ArrList.length){
+            resize(ArrList.length * 2);
+        }
+
+        System.arraycopy(ArrList, index, ArrList, index + 1, point - index);
+        ArrList[index] = element;
+        point++;
+
+    }
+
+    @Override
+    public E remove(int index){
+        E value = (E) ArrList[index];
+        for(int i = index; i < point; i++){ //первая версия
+            ArrList[i] = ArrList[i+1];
+        }
+      //  System.arraycopy(ArrList, index + 1, ArrList, index, point - index); // вторая версия
+        ArrList[point--] = null;
+
+        return value;
+    }
+
+    private void removeZero(int index) {
+        for(int i = index; i < point; i++){ //первая версия
+            ArrList[i] = ArrList[i+1];
+        }
+        ArrList[point--] = null;
+    }
+
 }
 
